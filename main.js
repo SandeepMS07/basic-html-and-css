662f42c464f4994ce80f641b6aa7511b58624619
Sandeep M S <78291182+SandeepMS07@users.noreply.github.com>
2021-07-17 09:00:57 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
  let myImg = document.querySelector('img');
  myImg.onclick = function() {
-     let mySrc = myImg.getAttribute('src');
+     let mySrc = myImg.getAttribute('src','google.png');
      if(mySrc == 'to-do-list-apps-1400x1050.png') {
          
      }
