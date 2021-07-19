f739b4723b30f7fcb5cc929bcec3dd9ab929c17b
Sandeep M S <78291182+SandeepMS07@users.noreply.github.com>
2021-07-19 22:58:50 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,33 @@
- 
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+let myImg = document.querySelector('img');
+
+myImg.onclick = function() {
+     let mySrc = myImg.getAttribute('src');
      if(mySrc == 'to-do-list-apps-1400x1050.png') {
          myImg.setAttribute('src','google.png');
      }else {
          myImg.setAttribute('src','to-do-list-apps-1400x1050.png');
      }
- 
+}
+
+function setUserName(){
+    let myName = prompt('Please enter your name. ');
+    if(!myName){
+        setUserName();
+    }else{
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Mozilla is cool,  ' + myName;
+    }
+}
+
+if(!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla is cool,  ' + storedName;
+}
+
+myButton.onclick = function() {
+    setUserName();
+}
